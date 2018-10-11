f871baee771eeec4154781894308f59cc6e4d83b
Maria Gaci <maria.gaci@epfl.ch>
2018-10-11 09:27:55 +0200

    fix: change routing approach
    To switch between the teacher and student view, a new route was specified in the app-routing file. However, the routing should be done through a query param called mode.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TeacherComponent } from './teacher/teacher.component';
-import { StudentComponent } from './student/student.component';
-import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      { path: 'teacher', component: TeacherComponent },
-      { path: 'student', component: StudentComponent },
-      { path: '**', redirectTo: '/student' }
-    ])
+    RouterModule.forRoot([])
   ],
   exports: [RouterModule]
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, filter, skipWhile, skipUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  mode = 'student';
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit () {
+    this.route.queryParamMap.pipe(
+      map(params => params.get('mode')),
+      skipWhile(mode => !Boolean(mode))
+    ).subscribe(mode => {
+      this.mode = mode;
+      console.log(this.mode);
+    });
+  }
+}
